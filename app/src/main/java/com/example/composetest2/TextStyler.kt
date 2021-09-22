65fe6ad716ad4d827ea80388b354c4efcc886dce
darkliself <4e486bux@gmail.com>
2021-09-22 18:12:52 +0300

    09_22_first_demo

--- app/src/main/java/com/example/composetest2/TextStyler.kt
+++ app/src/main/java/com/example/composetest2/TextStyler.kt
@@ -1,20 +1,11 @@
 package com.example.composetest2
 
-import android.icu.text.UnicodeFilter
-import android.media.MediaCodec
-import android.os.Build
-import androidx.annotation.RequiresApi
-import java.util.regex.Pattern
-
-@RequiresApi(Build.VERSION_CODES.N)
 fun main(args: Array<String>) {
     val t = TextStyler("s")
     t.switchToUnicode("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890", 0)
     t.switchToUnicode("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYy", 1)
     t.switchToUnicode("jghfdjghdf", 1)
 }
-
-
 
 class TextStyler(var text: String = "") {
     private val baseAlphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890"
