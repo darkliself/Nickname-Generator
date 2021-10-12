4892b4046ca089b6cc9e026681cf5cef505e68a8
darkliself <4e486bux@gmail.com>
2021-10-12 18:11:26 +0300

    new test view

--- app/src/main/java/com/example/composetest2/MainActivity.kt
+++ app/src/main/java/com/example/composetest2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composetest2
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,12 +12,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.VectorPainter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.example.composetest2.ui.theme.NickCreator1
 
 
@@ -32,6 +38,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Header(str: String) {
+
+
+    fun fromPxToDp(px: Float): Float {
+        val density = Resources.getSystem().displayMetrics.density
+        return px / density;
+    }
+
     Column(
         Modifier
             .fillMaxSize()
@@ -39,9 +52,10 @@ fun Header(str: String) {
         Row(
             Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.2f)
+                .height(80.dp)
                 .border(border = BorderStroke(1.dp, Color.Black))
                 .padding(top = 20.dp, end = 20.dp),
+
             horizontalArrangement = Arrangement.End
         ) {
             Box(
@@ -52,54 +66,26 @@ fun Header(str: String) {
             ) {
             }
         }
-        Row(
+        Box(
             Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.2f)
+                .height(100.dp)
                 .border(border = BorderStroke(1.dp, Color.Black)),
-            horizontalArrangement = Arrangement.Center
+            contentAlignment = Alignment.Center
+
         ) {
+
             Text(text = "Create\nyour perfect nickname", textAlign = TextAlign.Center)
+
         }
-        Row(
+        Box(
             Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.2f)
-                .border(border = BorderStroke(1.dp, Color.Black)), Arrangement.Center
-        ) {
-            Button(onClick = { /*TODO*/ }) {
-                Image(
-                    painter = painterResource(R.drawable.icon),
-                    contentDescription = "ok icon",
-                    Modifier.size(20.dp, 20.dp)
-                )
-                Text("some simple text")
-            }
-        }
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.2f)
-                .border(border = BorderStroke(1.dp, Color.Black)), Arrangement.Center
-        ) {
-            Button(onClick = { /*TODO*/ }) {
-                Image(
-                    painter = painterResource(R.drawable.icon),
-                    contentDescription = "ok icon",
-                    Modifier.size(20.dp, 20.dp)
-                )
-                Text("some simple text")
-            }
-        }
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.2f)
+                .height(100.dp)
                 .border(border = BorderStroke(1.dp, Color.Black)),
-
-            Arrangement.Center,
-
+            contentAlignment = Alignment.Center
         ) {
+
             Button(onClick = { /*TODO*/ }) {
                 Image(
                     painter = painterResource(R.drawable.icon),
@@ -108,6 +94,76 @@ fun Header(str: String) {
                 )
                 Text("some simple text")
             }
+        }
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .border(border = BorderStroke(1.dp, Color.Black)),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Button(onClick = { /*TODO*/ }) {
+                Image(
+                    painter = painterResource(R.drawable.icon),
+                    contentDescription = "ok icon",
+                    Modifier.size(20.dp, 20.dp)
+                )
+                Text("some simple text")
+            }
+        }
+
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .border(border = BorderStroke(1.dp, Color.Black)),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Button(onClick = { /*TODO*/ }) {
+                Image(
+                    painter = painterResource(R.drawable.icon),
+                    contentDescription = "ok icon",
+                    Modifier.size(20.dp, 20.dp)
+                )
+                Text("some simple text")
+            }
+
+        }
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .border(border = BorderStroke(1.dp, Color.Black)),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+            ) {
+                Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text("jhdjhsf", Modifier.zIndex(2F))
+                    Image(
+                        ImageVector.vectorResource(id = R.drawable.ic_rec_101),
+                        "some description",
+                        Modifier
+                            .size(180.dp, 75.dp)
+                            .zIndex(1f)
+                    )
+                    Image(
+                        ImageVector.vectorResource(id = R.drawable.ic_rec_101_1), "bk_101",
+                        Modifier
+                            .size(200.dp, 80.dp)
+                            .zIndex(0f)
+
+                    )
+                }
+
+            }
+
         }
     }
 }
