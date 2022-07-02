dcd226000a5e288e180f8a844728f68bae94d631
darkliself <4e486bux@gmail.com>
2022-07-02 12:31:21 +0300

    some fixes

--- app/src/main/java/com/example/textstyler/util/Playground.kt
+++ app/src/main/java/com/example/textstyler/util/Playground.kt
@@ -12,21 +12,14 @@ fun main(args: Array<String>) {
 
     )
 
-    for (i in 1..12) {
-        for (j in 1..99)
-        getItemCode(j, i)
+    for (i in 1..1000) {
+        generateURLMinimalAvatar("hello motherfucker$i")
     }
 }
 
-fun generateURLMinimalAvatar(nickname: String) {
+fun generateURLMinimalAvatar(nickname: String): String {
     var nicknameCode = nickname.hashCode().toString()
     // https://api.minimalavatars.com/lottie?base=0&eyes=1&hair=0&mouth=9&baseColor=%237d205e&hairColor=%23f573ff&eyesColor=%23f573ff&mouthColor=%23f573ff
-
-
-
-
-
-
     // 8 params
     var itaration = 0
     while (nicknameCode.length < 16) {
@@ -40,26 +33,18 @@ fun generateURLMinimalAvatar(nickname: String) {
     while (nicknameCode.length > 16) {
         nicknameCode = ((abs(nicknameCode.toLong()) / 10).toString())
     }
-
     val codeArray = nicknameCode.chunked(2)
-    println(codeArray)
-
-    val base = codeArray[0]
-    val eyes = codeArray[1]
-    val hair = codeArray[2]
-    val mouth = codeArray[3]
-    val baseColor = codeArray[4] // example : %23007A5E
-    val hairColor = codeArray[5]
-    val eyesColor = codeArray[6]
-    val mouthColor = codeArray[7]
-
-
-    var resultString = "https://api.minimalavatars.com/lottie?base=$base&eyes=$eyes&hair=$hair&mouth=$mouth&baseColor=$baseColor&hairColor=$hairColor&eyesColor=$eyesColor&mouthColor=$mouthColor"
-
-    println(resultString)
+    // println(codeArray)
+    val base = getItemCode(codeArray[0].toInt(), AvatarsData.base)
+    val eyes = getItemCode(codeArray[1].toInt(), AvatarsData.eyes)
+    val hair = getItemCode(codeArray[2].toInt(), AvatarsData.hair)
+    val mouth = getItemCode(codeArray[3].toInt(), AvatarsData.mouth)
+    val baseColor = AvatarsData.colorsList[getItemCode(codeArray[4].toInt(), 40)] // example : %23007A5E
+    val hairColor = AvatarsData.colorsList[getItemCode(codeArray[4].toInt(), 40)]
+    val eyesColor = AvatarsData.colorsList[getItemCode(codeArray[4].toInt(), 40)]
+    val mouthColor = AvatarsData.colorsList[getItemCode(codeArray[4].toInt(), 40)]
     println(nicknameCode)
-
-   //  println(getItemCode(4, 40))
+    return "https://api.minimalavatars.com/lottie?base=$base&eyes=$eyes&hair=$hair&mouth=$mouth&baseColor=$baseColor&hairColor=$hairColor&eyesColor=$eyesColor&mouthColor=$mouthColor"
 }
 
 private fun getItemCode(num: Int, itemsInList: Int): Int {
@@ -77,56 +62,52 @@ private fun getItemCode(num: Int, itemsInList: Int): Int {
     return result
 }
 
-private object MinimalAvatarsData {
+private object AvatarsData {
     val base = 5
     val hair = 11
     val eyes = 9
     val mouth = 11
     val colorsList = listOf(
-        "23FF966A",
-        "23FF5668",
-        "23FFEA71",
-        "2307C7AE",
-        "23FC264C",
-        "23F7FFF7",
-        "232B1039",
-        "23426EB7",
-        "23EFC636",
-        "23F4D19E",
-        "23352C21",
-        "23C68F46",
-        "23A66EE6",
-        "234F2789",
-        "2356FCB1",
-        "238CF4EC",
-        "2300BBF2",
-        "23007A5E",
-        "23a4de02",
-        "231e5631",
-        "234c9a2a",
-        "235A4CA4",
-        "23161C46",
-        "23BAC1E3",
-        "237D205E",
-        "23D1205E",
-        "237DF45E",
-        "23B36DD4",
-        "2361269B",
-        "23542e71",
-        "23fb3640",
-        "23fdca40",
-        "23ffc93c",
-        "2331326f",
-        "234aa54e",
-        "2376FFBA",
-        "231D4F7C",
-        "2329D3A2",
-        "237d205e",
-        "23f573ff",
-        "23f573ff",
+        "%23FF966A",
+        "%23FF5668",
+        "%23FFEA71",
+        "%2307C7AE",
+        "%23FC264C",
+        "%23F7FFF7",
+        "%232B1039",
+        "%23426EB7",
+        "%23EFC636",
+        "%23F4D19E",
+        "%23352C21",
+        "%23C68F46",
+        "%23A66EE6",
+        "%234F2789",
+        "%2356FCB1",
+        "%238CF4EC",
+        "%2300BBF2",
+        "%23007A5E",
+        "%23a4de02",
+        "%231e5631",
+        "%234c9a2a",
+        "%235A4CA4",
+        "%23161C46",
+        "%23BAC1E3",
+        "%237D205E",
+        "%23D1205E",
+        "%237DF45E",
+        "%23B36DD4",
+        "%2361269B",
+        "%23542e71",
+        "%23fb3640",
+        "%23fdca40",
+        "%23ffc93c",
+        "%2331326f",
+        "%234aa54e",
+        "%2376FFBA",
+        "%231D4F7C",
+        "%2329D3A2",
+        "%237d205e",
+        "%23f573ff",
+        "%23f573ff",
     )
-
-    var hex = java.lang.String.format("#%02x%02x%02x",255, 150, 106)
-    // var z =  colorsList[0].toColor()
-
 }
